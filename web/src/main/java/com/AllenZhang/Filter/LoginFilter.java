8716b5a08ce91f5d5d8cff9f4290837914500896
ByZYM <838826997@qq.com>
2018-01-09 18:39:16 +0800

    更新LoginFilter
    拦截非法请求

---
+++ web/src/main/java/com/AllenZhang/Filter/LoginFilter.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2017 by AllenZhang
+ */
+
+package com.AllenZhang.Filter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/*
+ * 登录验证过滤器
+ *
+ *
+ * */
+public class LoginFilter implements Filter {
+
+    private static final Log logger = LogFactory.getLog(LoginFilter.class);
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        /* 如果不是ajax测试连接方法 */
+        if (!httpRequest.getMethod().equalsIgnoreCase("options")) {
+            HttpSession session = httpRequest.getSession();
+
+            String account = (String) session.getAttribute("account");
+
+            //ajax session 过期处理
+            if (account == null) {
+                logger.info("session过期处理");
+
+                logger.info("请求重定向到/error/login");
+                httpResponse.sendRedirect("/error/login");
+                return;
+
+            }
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
