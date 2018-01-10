404138c608f8d67ffc700e0704397a4d84b9bd48
ByZYM <838826997@qq.com>
2018-01-10 22:53:34 +0800

    BookService接口
    编写部分BookService接口

---
+++ web/src/main/java/com/AllenZhang/Service/BookService.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018 by AllenZhang
+ */
+
+package com.AllenZhang.Service;
+
+
+import java.awt.print.Book;
+
+public interface BookService {
+
+    boolean insert(Book book);
+
+}
