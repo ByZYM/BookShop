d88cf582f0b2e882a3435916a93424a7a11d8b9c
ThreeWays <35328217+ThreeWays@users.noreply.github.com>
2018-01-12 09:13:42 +0800

    UserInfoController1.0

--- web/src/main/java/com/AllenZhang/controller/api/UserInfoController.java
+++ web/src/main/java/com/AllenZhang/controller/api/UserInfoController.java
@@ -1,11 +1,17 @@
 package com.AllenZhang.controller.api;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.AllenZhang.dto.MetaDto;
+import com.AllenZhang.entity.UserInfo;
+import com.AllenZhang.service.UserService;
+import com.AllenZhang.utils.Status;
 
 
 /**
@@ -15,6 +21,11 @@ import com.AllenZhang.dto.MetaDto;
 @Controller
 @RequestMapping(value="/userInfo")
 public class UserInfoController {
+	@Autowired
+	private UserService userService;
+	
+	private static final Log logger = LogFactory.getLog(UserInfoController.class);
+	
 	
 	/**
 	 * 获取用户详细信息
@@ -22,18 +33,28 @@ public class UserInfoController {
 	 * @return
 	 */
 	@RequestMapping(value="/get/{userId}")
-	public @ResponseBody MetaDto getUserInfo(@PathVariable Integer userId){
-		return null;
-		
+	public @ResponseBody MetaDto getUserInfo(@PathVariable("userId") Integer userId){
+		MetaDto metaDto=new MetaDto();
+		UserInfo userInfo=userService.getUserInfoByUserId(userId);
+		if(userInfo==null){
+			metaDto.setMessage("查找失败");
+			metaDto.setData(Status.FAILED);
+		}else{
+			metaDto.setMessage("查找成功");
+			metaDto.setData(Status.SUCCESS);
+			metaDto.setData(userInfo);
+		}
+		return metaDto;
 	}
 	
 	/**
-	 * 获取用户详细信息
+	 *修改用户详细信息
 	 * @param userId
 	 * @return
 	 */
 	@RequestMapping(value="/update/{userId}")
-	public @ResponseBody MetaDto updateUserInfo(@PathVariable Integer userId){
+	public @ResponseBody MetaDto updateUserInfo(@PathVariable("userId") Integer userId){
+		//修改用户详细信息
 		return null;
 		
 	}
